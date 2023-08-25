3f0636056e8b4f96a6ccad7863de758127dce2b4
Yna Eva <yna@kodakollectiv.com>
2023-08-25 14:59:50 +0800

    chore: precompile assets for staging deployment

---
+++ config/puma_production.rb
@@ -0,0 +1,20 @@
+workers 2
+
+threads 1, 6
+
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
+
+port        ENV.fetch("PORT") { 4000 }
+
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
+daemonize true
+
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
