15e71e18c9d554a998ee2a5ea974172a7a028fda
Yna Eva <yna@kodakollectiv.com>
2023-08-25 15:24:35 +0800

    fix: config port for deploy

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -5,7 +5,7 @@ threads 1, 6
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-port        ENV.fetch("PORT") { 4000 }
+port        ENV.fetch("PORT") { 5000 }
 
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
